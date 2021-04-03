5b130875ccdc80053a1cfb0789a0f8c936b58ce8
Rui Vieira <ruidevieira@googlemail.com>
2021-04-03 01:32:31 +0100

    Add support for PNG output

--- src/vesta.cr
+++ src/vesta.cr
@@ -1,13 +1,16 @@
 require "cmark"
 require "icr"
+require "json"
+require "string_scanner"
 
 module Vesta
   class Renderer
     property options : Cmark::Option
 
-    def initialize(@file_path : String)
+    def initialize(@file_path : String, @image_dest : String = "images", @image_prefix : String = "image")
       @options = Cmark::Option.flags(Nobreaks, ValidateUTF8) # deafult is Option::None
       extensions = Cmark::Extension.flags(Table, Tasklist)   # default is Extension::None
+      @image_counter = 1
 
       @md = File.read(file_path)
 
@@ -22,11 +25,33 @@ module Vesta
           command.push node.literal
           executer = Icr::Executer.new command
           result = executer.execute
-          result = Cmark::NodeMaker.code_block(result.value.to_s)
-          node.insert_after(result)
-        else
+          value = result.value.to_s
+          if is_png?(value)
+            image_path = "#{@image_dest}/#{@image_prefix}-#{@image_counter}.png"
+            save_png_data(image_path, value)
+            image_link = Cmark::NodeMaker.image(image_path, "#{@image_prefix}-#{@image_counter}")
+            next_node = node.next.not_nil!
+            next_node.prepend_child(Cmark::NodeMaker.linebreak)
+            next_node.prepend_child(image_link)
+            @image_counter += 1
+          else
+            output = Cmark::NodeMaker.code_block(value)
+            node.insert_after(output)
+          end
         end
       end
+    end
+
+    def save_png_data(image_path : String, data : String)
+      command = Icr::CommandStack.new
+      command.push "File.write(\"#{image_path}\", " + data + ")"
+      @image_counter += 1
+      executer = Icr::Executer.new command
+      executer.execute
+    end
+
+    def is_png?(data : String)
+      return data[1..7] == "\\x89PNG"
     end
 
     def render : String
